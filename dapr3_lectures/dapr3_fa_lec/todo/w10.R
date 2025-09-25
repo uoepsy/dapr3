3dba48f64f56dd34d949184a7b5e79acba7f42b2
Josiah King <josiahpjking@gmail.com>
2025-09-25 15:31:23 +0100

    lab sols

---
+++ dapr3_lectures/dapr3_fa_lec/todo/w10.R
@@ -0,0 +1,17 @@
+
+
+
+# get students to measure 2 properties for 10 things
+   # - e.g., length and angles of lines
+  # split in two groups. one with fewer increments on scale
+
+# then get them to rate set of properties of group of people
+
+# go back to thing A, ask them to measure again
+
+#  test re-test
+# correlation thing A measurements, split by group
+
+
+# inter rater
+# rating of properties across raters
