9125aa1d7e03daf693dfed9d8271dc7689cb4708
Josiah King <josiahpjking@gmail.com>
2026-05-22 11:11:32 +0100

    bit more block 2

---
+++ dapr3_flashcards/layou.R
@@ -0,0 +1,21 @@
+# concepts
+# - core methods
+# - dimensionality of data
+# - var,sd,cov,cor
+# - graphical models
+# - rotations in EFA
+# - "model fit" in CFA
+# - reliability
+# - validity
+# 
+# 
+# how to
+# - wrangle questionnaire data
+# - explore questionnaire data (cor matrix, heatmap, skew etc)
+# - calculate a scale score
+# - the steps of PCA
+# - the steps of EFA
+# - decide on the number of components or factors
+# - read PCA/EFA outputs
+# - the steps of CFA
+# - estimate reliability
